e1fa312b34cf4df18d97f94b504b4d5c0727aded
amir <nobody76911@gmail.com>
2026-04-26 19:22:04 +0330

    third commit successful

--- Transfer/urls.py
+++ Transfer/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webb import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("Hi/",views.index,name="index"),
 ]
