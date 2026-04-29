447b8a95dc1ee27e26022ede5b733286b6783b55
amir <nobody76911@gmail.com>
2026-04-29 19:03:15 +0330

    sixth commit successful. made a model named Human with two fields called first name and last name

--- webb/models.py
+++ webb/models.py
@@ -1,3 +1,12 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+class Human(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name +" " + self.last_name    
+
+
