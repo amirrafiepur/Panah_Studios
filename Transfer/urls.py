c7d1af60554dbc3ef90de89a2875e88e2d93eb47
amir <nobody76911@gmail.com>
2026-05-18 01:46:31 +0330

    final version of the project with hand-made front

--- Transfer/urls.py
+++ Transfer/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webb import views  
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.index,name='index'),
-    path("formpage/",views.index_addHuman,name="index_addHuman")
+    path('first-page',include('webb.urls')),
 ] 
