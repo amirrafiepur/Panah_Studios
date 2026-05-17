19c8e15e8676d13bd29e349a8bd6269e0cc2a5d7
amir <nobody76911@gmail.com>
2026-05-18 03:28:05 +0330

    added a downloaded front template that doesnt work yet

--- webb/views.py
+++ webb/views.py
@@ -1,11 +1,23 @@
 # Create your views here.
 
 from django.shortcuts import render
-from webb.models import Human
+from webb.models import Human  #TODO:add a table in index.html to show the table Human in database
 
 
 def index(request):
-    table_data=Human.objects.all()
-    my_dic={"data" : table_data}
-    return render(request,'webb/index.html',context=my_dic)
+    return render(request,'webb/index.html')
+def index_portfolio_kenburns(request):
+    return render(request,'webb/index-portfolio-kenburns.html')
+def index_portfolio_image(request):
+    return render(request,'webb/index-portfolio-image.html')
+def index_countdown_slider(request):
+    return render(request,'webb/index-countdown-slider.html')
+def index_countdown_particles_image(request):
+    return render(request,'webb/index-countdown-particles-image.html')
+def index_agency_video(request):
+    return render(request,'webb/index-agency-video.html')
+def index_agency_triangles_slider(request):
+    return render(request,'webb/index-agency-triangles-slider.html')
 
+def test(request):
+    return render(request,'webb/test.html')
