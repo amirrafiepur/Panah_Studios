17e6a77dada7ed753f7fea36d0273215ac34cf46
amir <nobody76911@gmail.com>
2026-05-19 05:06:01 +0330

    added 4 completed templates. back,front,contact and home.

--- webb/urls.py
+++ webb/urls.py
@@ -4,5 +4,8 @@ from webb.views import *
 
 
 urlpatterns = [
-    path('',index,name='index'),
+    path('home/',index,name='home'),
+    path('contact/',contact,name="contact"),
+    path('front-end/',front,name='front'),
+    path('back-end/',back,name='back')
 ] 
--- webb/views.py
+++ webb/views.py
@@ -6,15 +6,23 @@ from webb.models import Human  #TODO:add a table in index.html to show the table
 
 def index(request):
     return render(request,'webb/index.html')
+
+def back(request):
+    return render(request,'webb/back.html')
+
+def front(request):
+    return render(request,'webb/front.html')
+
+def contact(request):
+    return render(request,'webb/contact.html')
+
+
+
 def index_portfolio_kenburns(request):
     return render(request,'webb/index-portfolio-kenburns.html')
-def index_portfolio_image(request):
-    return render(request,'webb/index-portfolio-image.html')
+
 def index_countdown_slider(request):
     return render(request,'webb/index-countdown-slider.html')
-def index_countdown_particles_image(request):
-    return render(request,'webb/index-countdown-particles-image.html')
+
 def index_agency_video(request):
     return render(request,'webb/index-agency-video.html')
-def index_agency_triangles_slider(request):
-    return render(request,'webb/index-agency-triangles-slider.html')
