62293dfbc838d50b109c1b13f34af3b492cdccae
amirrafiepur <nobody76911@gmail.com>
2026-06-10 22:06:40 +0330

    added three test tags to blog_tags.py
    +added a test simple_tag to blog_tags.py
    +added a test filter_tag to blog_tags.py
    +added a test inclusion_tag to blog_tags.py with a test.html template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,16 @@
 from django import template 
-
+from blog.models import * 
 register = template.Library()
 
 @register.simple_tag
 def tag_name():
     return 'This is a simple tag'
+
+@register.filter
+def filter_tag(data): 
+    return 3 + data
+
+@register.inclusion_tag('blog/test.html')
+def inclusion_tag():
+    posts = post.objects.filter(status=1)
+    return {'posts':posts}
